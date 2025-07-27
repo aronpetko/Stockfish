a1867f56c465f14881fcacdc20ddb9f0f7d85c10
aronpetkovski <aronpetko@gmail.com>
2025-07-27 13:41:59 +0200

    Bench: 2791044

--- src/search.cpp
+++ src/search.cpp
@@ -820,8 +820,8 @@ Value Search::Worker::search(
 
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 4 && !(ss - 1)->ttPv && ss->ttPv)
-        depth++;
+    if (priorReduction <= 3 && !(ss - 1)->ttPv && ss->ttPv && !PvNode)
+        depth--;
     if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 175)
         depth--;
 
