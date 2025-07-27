7c993eb5184d0e5dc4b2ec343050e95e88b3346f
aronpetkovski <aronpetko@gmail.com>
2025-07-27 23:10:59 +0200

    Bench: 2547549

--- src/search.cpp
+++ src/search.cpp
@@ -764,6 +764,10 @@ Value Search::Worker::search(
         }
     }
 
+    const PieceToHistory* contHist[] = {
+      (ss - 1)->continuationHistory, (ss - 2)->continuationHistory, (ss - 3)->continuationHistory,
+      (ss - 4)->continuationHistory, (ss - 5)->continuationHistory, (ss - 6)->continuationHistory};
+
     // Step 6. Static evaluation of the position
     Value      unadjustedStaticEval = VALUE_NONE;
     const auto correctionValue      = correction_value(*this, pos, ss);
@@ -903,9 +907,7 @@ Value Search::Worker::search(
         // If value from transposition table is lower than probCutBeta, don't attempt
         // probCut there
         && !(is_valid(ttData.value) && ttData.value < probCutBeta)
-        && (!ttData.move
-            || pawnHistory[pawn_structure_index(pos)][pos.moved_piece(ttData.move)][move.to_sq()]
-                 > 5000))
+        && (!ttData.move || (*contHist[0])[pos.moved_piece(ttData.move)][move.to_sq()] > 5000))
     {
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
@@ -960,10 +962,6 @@ moves_loop:  // When in check, search starts here
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
-
-    const PieceToHistory* contHist[] = {
-      (ss - 1)->continuationHistory, (ss - 2)->continuationHistory, (ss - 3)->continuationHistory,
-      (ss - 4)->continuationHistory, (ss - 5)->continuationHistory, (ss - 6)->continuationHistory};
 
 
     MovePicker mp(pos, ttData.move, depth, &mainHistory, &lowPlyHistory, &captureHistory, contHist,
