5dacb3b90befe361f42cdd43d51cd6a22c926e62
aronpetkovski <aronpetko@gmail.com>
2025-07-27 23:27:22 +0200

    Bench: 2253055

--- src/search.cpp
+++ src/search.cpp
@@ -1076,7 +1076,7 @@ moves_loop:  // When in check, search starts here
 
                 Value baseFutility = (bestMove ? 46 : 230);
                 Value futilityValue =
-                  ss->staticEval + baseFutility + 117 * lmrDepth + 102 * (ss->staticEval > alpha);
+                  ss->staticEval + baseFutility + 117 * lmrDepth + 102 * (eval > alpha);
 
                 // Futility pruning: parent node
                 // (*Scaler): Generally, more frequent futility pruning
