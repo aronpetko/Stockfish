011dc2c7172d7faeb463052720bec738b145c73f
aronpetkovski <aronpetko@gmail.com>
2025-07-27 23:30:00 +0200

    Bench: 2813984

--- src/search.cpp
+++ src/search.cpp
@@ -1187,7 +1187,10 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes
         if (cutNode)
+        {
             r += 3000;
+            r += 800 * (priorReduction >= 4);
+        }
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
